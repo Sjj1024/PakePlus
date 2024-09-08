1c8da3b5bc9015cf7831d5779c98051a9cf134db
1024小神 <15670339118@163.com>
2024-09-08 15:25:30 +0800

    add gride

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,13 +1,121 @@
 <template>
     <div class="homeBox">
         <div class="homeHeader">
-            <div>APP管理</div>
-            <div>编辑，调试，管理</div>
+            <div class="headerTitle">APP管理</div>
+            <div class="headerTool">
+                <div class="toolTips">创建，编辑，调试APP</div>
+                <div class="control">管理</div>
+            </div>
         </div>
-        <div class="projectBox">APP内容</div>
+        <div class="projectBox">
+            <!-- 已有项目列表 -->
+            <div class="project" v-for="i in 8" :key="i">
+                <img
+                    src="https://hadoappusage.oss-cn-shanghai.aliyuncs.com/static/pad_image/blueBg.png"
+                    class="appIcon"
+                    alt="appIcon"
+                />
+                <div class="infoBox">
+                    <div class="appBox">
+                        <div class="appName">测试阮家</div>
+                        <div class="appVersion">1.01</div>
+                    </div>
+                    <div class="appDesc">我的第一个让UN就爱你</div>
+                </div>
+            </div>
+            <!-- 新建按钮 -->
+            <div class="newProject">
+                <el-icon><Plus /></el-icon>
+            </div>
+        </div>
     </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
 
-<style lang="scss" scoped></style>
+const appList = ref([
+    {
+        appName: '测试阮家',
+        appIcon:
+            'https://hadoappusage.oss-cn-shanghai.aliyuncs.com/static/pad_image/blueBg.png',
+        appVersion: '1.0',
+        appResp: 'https://github.com/',
+        appUrl: 'https://www.csdn.net/',
+        appId: 'sdfafsasofja',
+        appDesc: '我的第一个让UN就爱你',
+    },
+])
+</script>
+
+<style lang="scss" scoped>
+.homeBox {
+    .homeHeader {
+        .headerTitle {
+            font-size: large;
+            font-weight: bold;
+        }
+
+        .headerTool {
+            display: flex;
+            flex-direction: row;
+            justify-content: space-between;
+
+            .toolTips {
+                color: gray;
+            }
+
+            .control {
+                color: #2a598a;
+                cursor: pointer;
+            }
+        }
+    }
+
+    .projectBox {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+        grid-gap: 10px;
+        gap: 10px;
+        margin-top: 10px;
+
+        .project {
+            width: 100%;
+            height: 200px;
+            border-radius: 5px;
+            border: 1px solid gray;
+            margin-right: 10px;
+            margin-bottom: 10px;
+
+            .appIcon {
+                width: 100%;
+                height: 66%;
+            }
+
+            .infoBox {
+                padding: 5px;
+                .appBox {
+                    display: flex;
+                    flex-direction: row;
+                    justify-content: space-between;
+                }
+
+                .appDesc {
+                    font-size: small;
+                    color: gray;
+                }
+            }
+        }
+        .newProject {
+            width: 100%;
+            height: 200px;
+            border-radius: 5px;
+            border: 1px solid gray;
+            display: flex;
+            flex-direction: row;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,11 @@
 import { createMemoryHistory, createRouter } from 'vue-router'
 import Home from '@/pages/home.vue'
+import Edit from '@/pages/edit.vue'
 import About from '@/pages/about.vue'
 
 const routes = [
     { path: '/', component: Home },
+    { path: '/edit', component: Edit },
     { path: '/about', component: About },
 ]
 
