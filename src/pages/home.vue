51c72bee99f74ae82c6ce14a007edf794816a3ea
1024小神 <15670339118@163.com>
2024-10-09 10:03:38 +0800

    update ui

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -543,7 +543,7 @@ onMounted(() => {
                 width: 100%;
                 height: 66%;
                 object-fit: cover;
-                border-radius: 5px;
+                border-radius: 5px 5px 0 0;
             }
 
             .appPreview {
