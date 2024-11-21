35bed3ed4395583c974a51301da7e4556e5b4fed
1024小神 <15670339118@163.com>
2024-11-21 10:35:22 +0800

    juejin auto single

--- docs/custom.js
+++ docs/custom.js
@@ -1,3 +1,5 @@
+let isSignin = localStorage.getItem('isSignin') === 'true'
+
 document.addEventListener('DOMContentLoaded', () => {
     const targetNode = document.body
     // 配置观察选项
@@ -6,17 +8,37 @@ document.addEventListener('DOMContentLoaded', () => {
         subtree: true,
     }
     const observer = new MutationObserver((mutationsList, observer) => {
-        console.log('juejin auto signin ')
-        const singleBtn = document.querySelector('.signin-btn')
-        if (singleBtn && singleBtn.innerText === '签到') {
-            singleBtn.click()
-        }
-        // click
-        const signinBtn = document.querySelector(
-            'div.code-calender > button.signin'
+        // if login, then signin
+        const loginBtn = document.querySelector(
+            'li.nav-item.auth > div > button'
         )
-        if (signinBtn && signinBtn.innerText === '签到') {
-            signinBtn.click()
+        if (loginBtn && loginBtn.innerText.includes('登录')) {
+            console.log('user nedd login')
+            return
+        } else {
+            const singleBtn = document.querySelector('.signin-btn')
+            console.log('juejin auto signin', singleBtn, isSignin)
+            if (
+                singleBtn &&
+                singleBtn.innerText.includes('去签到') &&
+                !isSignin
+            ) {
+                singleBtn.click()
+            }
+            // click
+            const signinBtn = document.querySelector(
+                'div.code-calender > button.signin'
+            )
+            console.log('juejin auto signin', signinBtn)
+            if (
+                signinBtn &&
+                signinBtn.innerText.includes('立即签到') &&
+                !isSignin
+            ) {
+                signinBtn.click()
+                isSignin = true
+                localStorage.setItem('isSignin', 'true')
+            }
         }
     })
     observer.observe(targetNode, config)
