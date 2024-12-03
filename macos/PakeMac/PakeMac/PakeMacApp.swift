17aaa3838059c68281e6b221202197e399273b58
1024小神 <15670339118@163.com>
2024-12-03 16:01:24 +0800

    add mac pakeplus

---
+++ macos/PakeMac/PakeMac/PakeMacApp.swift
@@ -0,0 +1,17 @@
+//
+//  PakeMacApp.swift
+//  PakeMac
+//
+//  Created by song on 2024/12/3.
+//
+
+import SwiftUI
+
+@main
+struct PakeMacApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
