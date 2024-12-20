35bae8ede0e895dd25b21a980402bbc0b1068ae4
More or less? <73693935+lxl-sql@users.noreply.github.com>
2024-12-20 10:18:15 +0800

    update main rust from pakeplus

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,11 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+use serde_json::Error;
+use tauri::{utils::config::WindowConfig, Menu, MenuItem, Submenu, WindowBuilder};
 
-use tauri::{Menu, MenuItem, Submenu};
-
-// 对command单独管理
-mod command;
+fn json_to_window_config(window_json: &str) -> Result<WindowConfig, Error> {
+    serde_json::from_str(window_json)
+}
 
 fn main() {
     #[cfg(any(target_os = "linux", target_os = "windows"))]
@@ -23,19 +24,23 @@ fn main() {
             .add_native_item(MenuItem::Quit),
     ));
     tauri::Builder::default()
+        .setup(|app| {
+            let app_handle = app.handle();
+            let window_json = r#"{"label":"NiKaAI","url":"https://nika-public.com/","userAgent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36","fileDropEnabled":true,"center":true,"width":800,"height":600,"minWidth":null,"minHeight":null,"maxWidth":null,"maxHeight":null,"resizable":true,"maximizable":true,"minimizable":true,"closable":true,"title":"NiKa AI","fullscreen":false,"focus":false,"transparent":false,"maximized":false,"visible":true,"decorations":true,"alwaysOnTop":false,"contentProtected":false,"skipTaskbar":false,"titleBarStyle":"Visible","hiddenTitle":false,"acceptFirstMouse":false,"tabbingIdentifier":"","additionalBrowserArgs":""}"#;
+            match json_to_window_config(window_json) {
+                Ok(config) => {
+                    println!("Parsed WindowConfig: {:?}", config);
+                    let _main_window = WindowBuilder::from_config(&app_handle, config)
+                        .build()
+                        .unwrap();
+                }
+                Err(err) => {
+                    eprintln!("Failed to parse JSON: {}", err);
+                }
+            }
+            Ok(())
+        })
         .menu(menu)
-        .invoke_handler(tauri::generate_handler![
-            command::pakeplus::open_window,
-            command::pakeplus::preview_from_config,
-            command::pakeplus::update_build_file,
-            command::pakeplus::update_config_file,
-            command::pakeplus::update_cargo_file,
-            command::pakeplus::update_main_rust,
-            command::pakeplus::update_custom_js,
-            command::pakeplus::content_to_base64,
-            command::pakeplus::update_config_json,
-            command::pakeplus::rust_main_window,
-        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
