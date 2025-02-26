abebbe3d55dcca68af901e6bad5e672a48e7b881
John7125 <72534153+John7125@users.noreply.github.com>
2025-02-26 19:56:57 +0800

    update lib rust from pakeplus

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,6 @@
 mod command;
 use serde_json::json;
-use tauri::{menu::*, Manager, WindowEvent};
+use tauri::{menu::*, WindowEvent};
 use tauri_plugin_store::StoreExt;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -30,12 +30,12 @@ pub fn run() {
         })
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_os::init())
-        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             command::pakeplus::open_window,
             command::pakeplus::preview_from_config,
@@ -43,30 +43,37 @@ pub fn run() {
             command::pakeplus::update_config_file,
             command::pakeplus::update_cargo_file,
             command::pakeplus::update_main_rust,
-            command::pakeplus::rust_lib_window,
             command::pakeplus::update_custom_js,
             command::pakeplus::content_to_base64,
             command::pakeplus::update_config_json,
+            command::pakeplus::rust_lib_window,
             command::pakeplus::rust_main_window,
         ])
         .setup(|app| {
-            let window = app.get_webview_window("main").unwrap();
-            // This loads the store from disk
+            let app_handle = app.handle();
+            let main_window = tauri::WebviewWindowBuilder::from_config(
+                app_handle,
+                &app.config().app.windows.get(0).unwrap().clone(),
+            )
+            .unwrap()
+            .initialization_script(include_str!("./extension/event.js"))
+            .initialization_script(include_str!("./extension/custom.js"))
+            .build()
+            .unwrap();
             let store = app.store("app_data.json")?;
             let window_size: Option<serde_json::Value> = store.get("window_size");
             if let Some(window_size) = window_size {
                 let size = window_size.as_object().unwrap();
                 let width = size["width"].as_f64().unwrap();
                 let height = size["height"].as_f64().unwrap();
-                println!("window size init: {:?}", size);
-                window
+                // println!("window size init: {:?}", size);
+                main_window
                     .set_size(tauri::PhysicalSize::new(width, height))
                     .unwrap();
             }
-            // listen window sizi change event
-            window.on_window_event(move |event| {
+            main_window.on_window_event(move |event| {
                 if let WindowEvent::Resized(size) = event {
-                    println!("window resized: {:?}", size);
+                    // println!("window resized: {:?}", size);
                     let _ = store.set(
                         "window_size",
                         json!({
@@ -76,7 +83,6 @@ pub fn run() {
                     );
                 }
             });
-
             Ok(())
         })
         .run(tauri::generate_context!())
