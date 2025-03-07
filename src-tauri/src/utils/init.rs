3fe8aba35c9562367e1e8e61c6f6a25c76f4106c
1024小神 <15670339118@163.com>
2025-03-07 23:27:22 +0800

    Enhance window management with persistent state
    - Add support for saving and restoring window fullscreen state
    - Implement window position tracking and restoration
    - Improve window initialization logic with more robust state handling
    - Add checks to prevent saving invalid window sizes and positions

--- src-tauri/src/utils/init.rs
+++ src-tauri/src/utils/init.rs
@@ -5,20 +5,42 @@ use tauri_plugin_store::StoreExt;
 // handle something when start app
 pub async fn resolve_setup(app: &mut App) -> Result<(), Error> {
     let app_handle = app.handle();
-    // 获取记录窗口大小
-    let store = app.store("app_data.json").unwrap();
-    let window_size: Option<serde_json::Value> = store.get("window_size");
-    println!("windows_size: {:?}", window_size);
     // 示例 JSON 字符串
     let window_json = r#"
         {
             "title": "PakePlus",
             "center": true
         }
-    "#
-    .to_string();
+    "#;
+    // 解析 JSON 字符串为 WindowConfig 类型
+    let config: WindowConfig = serde_json::from_str(window_json).unwrap();
+    let window = tauri::WebviewWindowBuilder::from_config(app_handle, &config)
+        .unwrap()
+        .build()
+        .unwrap();
+
+    // 获取记录窗口大小
+    let store = app.store("app_data.json").unwrap();
+
+    // 获取记录窗口全屏
+    let window_fullscreen: Option<serde_json::Value> = store.get("window_fullscreen");
+    println!("windows_fullscreen: {:?}", window_fullscreen);
+    // 如果window_fullscreen存在，则设置全屏
+    if let Some(window_fullscreen) = window_fullscreen {
+        let fullscreen = window_fullscreen.as_bool().unwrap();
+        println!("fullscreen: {:?}", fullscreen);
+        if fullscreen {
+            window.set_fullscreen(true).unwrap();
+            println!("window fullscreen");
+        }
+    }
+
+    // 获取记录窗口大小
+    let window_size: Option<serde_json::Value> = store.get("window_size");
+    println!("windows_size: {:?}", window_size);
     let mut width = 800.0;
     let mut height = 600.0;
+    // 如果window_size存在，则设置窗口大小
     if let Some(window_size) = window_size {
         let size = window_size.as_object().unwrap();
         width = size["width"].as_f64().unwrap();
@@ -26,14 +48,31 @@ pub async fn resolve_setup(app: &mut App) -> Result<(), Error> {
         println!("width: {:?}", width);
         println!("height: {:?}", height);
     }
-    // 解析 JSON 字符串为 WindowConfig 类型
-    let config: WindowConfig = serde_json::from_str(&window_json).unwrap();
-    // 打印解析结果
-    // println!("{:#?}", config);
-    let window = tauri::WebviewWindowBuilder::from_config(app_handle, &config)
-        .unwrap()
-        .build()
-        .unwrap();
+    // 获取记录窗口位置
+    let window_position: Option<serde_json::Value> = store.get("window_position");
+    let mut x = 0.0;
+    let mut y = 0.0;
+    println!("windows_position: {:?}", window_position);
+    // 如果window_position存在，则设置窗口位置
+    if let Some(window_position) = window_position {
+        let position = window_position.as_object().unwrap();
+        x = position["x"].as_f64().unwrap();
+        y = position["y"].as_f64().unwrap();
+        println!("x: {:?}", x);
+        println!("y: {:?}", y);
+    }
+
+    let window_clone = window.clone();
+    // 设置窗口位置
+    // 如果xy为0，则窗口为中心位置
+    if x == 0.0 && y == 0.0 {
+        window.center().unwrap();
+    } else {
+        window
+            .set_position(tauri::PhysicalPosition::new(x, y))
+            .unwrap();
+    }
+
     // 设置窗口大小
     window
         .set_size(tauri::PhysicalSize::new(width, height))
@@ -42,17 +81,47 @@ pub async fn resolve_setup(app: &mut App) -> Result<(), Error> {
     window.on_window_event(move |event| {
         if let WindowEvent::Resized(size) = event {
             // println!("window_size: {:?}", size);
-            let _ = store.set(
-                "window_size",
-                json!({
-                    "width": size.width,
-                    "height": size.height
-                }),
-            );
+            if size.width > 0 && size.height > 0 {
+                let _ = store.set(
+                    "window_size",
+                    json!({
+                        "width": size.width,
+                        "height": size.height
+                    }),
+                );
+            }
+            if window_clone.is_fullscreen().unwrap_or(false) {
+                println!("Window entered fullscreen mode.");
+                let _ = store.set(
+                    "window_fullscreen",
+                    json!({
+                        "fullscreen": true
+                    }),
+                );
+            } else {
+                println!("Window exited fullscreen mode.");
+                let _ = store.set(
+                    "window_fullscreen",
+                    json!({
+                        "fullscreen": false
+                    }),
+                );
+            }
+        }
+        // 监听窗口位置变化
+        if let WindowEvent::Moved(position) = event {
+            println!("window_position: {:?}", position);
+            if position.x > 0 && position.y > 0 {
+                let _ = store.set(
+                    "window_position",
+                    json!({ "x": position.x, "y": position.y }),
+                );
+            }
         }
     });
 
     // 显示窗口
     window.show().unwrap();
+
     Ok(())
 }
