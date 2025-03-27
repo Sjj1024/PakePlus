d61f7a9001913ad3ed3616c512fdf9dbe772744c
1024小神 <15670339118@163.com>
2025-03-27 11:23:37 +0800

    Update file paths in copyStatic function to reflect the correct directory structure for index HTML files

--- cli/gitpress.cjs
+++ cli/gitpress.cjs
@@ -18,19 +18,19 @@ function copyStatic() {
     const staticPath = path.join(__dirname, '../docs/static')
     const distStaticPath = path.join(__dirname, '../docs/dist/static')
     fs.cpSync(staticPath, distStaticPath, { recursive: true })
-    // 将index_zh.html 复制到 dist/
-    // 将index_en.html 复制到 dist/
-    // 将index_ja.html 复制到 dist/
+    // 将docs/index_zh.html 复制到 docs/dist/
+    // 将docs/index_en.html 复制到 docs/dist/
+    // 将docs/index_ja.html 复制到 docs/dist/
     fs.copyFileSync(
-        path.join(staticPath, 'index_zh.html'),
+        path.join('../docs', 'index_zh.html'),
         path.join('../docs/dist', 'index_zh.html')
     )
     fs.copyFileSync(
-        path.join(staticPath, 'index_en.html'),
+        path.join('../docs', 'index_en.html'),
         path.join('../docs/dist', 'index_en.html')
     )
     fs.copyFileSync(
-        path.join(staticPath, 'index_ja.html'),
+        path.join('../docs', 'index_ja.html'),
         path.join('../docs/dist', 'index_ja.html')
     )
 }
