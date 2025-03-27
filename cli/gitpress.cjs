180a15ddcd72577bb7ab01f451cac39da6dcdb3c
1024小神 <15670339118@163.com>
2025-03-27 11:06:15 +0800

    Add functionality to copy multiple language index HTML files to the distribution directory in the copyStatic function

--- cli/gitpress.cjs
+++ cli/gitpress.cjs
@@ -19,6 +19,21 @@ function copyStatic() {
     const staticPath = path.join(__dirname, '../docs/static')
     const distStaticPath = path.join(__dirname, '../docs/dist/static')
     fs.cpSync(staticPath, distStaticPath, { recursive: true })
+    // 将index_zh.html 复制到 dist/
+    // 将index_en.html 复制到 dist/
+    // 将index_ja.html 复制到 dist/
+    fs.copyFileSync(
+        path.join(staticPath, 'index_zh.html'),
+        path.join(distStaticPath, 'index_zh.html')
+    )
+    fs.copyFileSync(
+        path.join(staticPath, 'index_en.html'),
+        path.join(distStaticPath, 'index_en.html')
+    )
+    fs.copyFileSync(
+        path.join(staticPath, 'index_ja.html'),
+        path.join(distStaticPath, 'index_ja.html')
+    )
 }
 
 // 根据参数 修改vitepress base 或者 copy /docs/static 到 /docs/dist/static
