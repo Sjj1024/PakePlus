6d1c1226f28fb2f5fcb4290fc45c41a0afab2834
1024小神 <15670339118@163.com>
2025-03-27 11:14:14 +0800

    Comment out static file copying logic in copyStatic function for future reference

--- cli/gitpress.cjs
+++ cli/gitpress.cjs
@@ -15,24 +15,24 @@ function gitBase() {
 
 function copyStatic() {
     console.log('copyStatic')
-    const staticPath = path.join(__dirname, '../docs/static')
-    const distStaticPath = path.join(__dirname, '../docs/dist/static')
-    fs.cpSync(staticPath, distStaticPath, { recursive: true })
-    // 将index_zh.html 复制到 dist/
-    // 将index_en.html 复制到 dist/
-    // 将index_ja.html 复制到 dist/
-    fs.copyFileSync(
-        path.join(staticPath, 'index_zh.html'),
-        path.join('../docs/dist', 'index_zh.html')
-    )
-    fs.copyFileSync(
-        path.join(staticPath, 'index_en.html'),
-        path.join('../docs/dist', 'index_en.html')
-    )
-    fs.copyFileSync(
-        path.join(staticPath, 'index_ja.html'),
-        path.join('../docs/dist', 'index_ja.html')
-    )
+    // const staticPath = path.join(__dirname, '../docs/static')
+    // const distStaticPath = path.join(__dirname, '../docs/dist/static')
+    // fs.cpSync(staticPath, distStaticPath, { recursive: true })
+    // // 将index_zh.html 复制到 dist/
+    // // 将index_en.html 复制到 dist/
+    // // 将index_ja.html 复制到 dist/
+    // fs.copyFileSync(
+    //     path.join(staticPath, 'index_zh.html'),
+    //     path.join('../docs/dist', 'index_zh.html')
+    // )
+    // fs.copyFileSync(
+    //     path.join(staticPath, 'index_en.html'),
+    //     path.join('../docs/dist', 'index_en.html')
+    // )
+    // fs.copyFileSync(
+    //     path.join(staticPath, 'index_ja.html'),
+    //     path.join('../docs/dist', 'index_ja.html')
+    // )
 }
 
 // 根据参数 修改vitepress base 或者 copy /docs/static 到 /docs/dist/static
