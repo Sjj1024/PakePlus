6d8e1a7c9650880043a929414014a87c3bbfb82c
1024小神 <15670339118@163.com>
2025-03-31 13:17:49 +0800

    Add vue-gtag dependency and initialize Google Analytics in main.ts for tracking user interactions.

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import { createPinia } from 'pinia'
 import i18n from './lang/index'
+import { createGtag } from 'vue-gtag'
 import '@/assets/fonts/iconfont.css'
 import 'element-plus/dist/index.css'
 import 'element-plus/theme-chalk/dark/css-vars.css'
@@ -15,5 +16,8 @@ const pinia = createPinia()
 app.use(router)
 app.use(pinia)
 app.use(i18n)
+createGtag({
+    tagId: 'G-7ZK1NBKC95',
+})
 
 app.mount('#app')
