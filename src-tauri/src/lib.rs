bcc4e54246f4ea89fb54d2137ebdc1aeab40de5b
1024小神 <15670339118@163.com>
2025-05-15 20:18:42 +0800

    Add tauri-plugin-updater to dependencies and update Cargo.lock: Introduced new plugin for application updates, added necessary dependencies, and ensured compatibility with existing packages.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -40,6 +40,7 @@ pub fn run() {
         .plugin(tauri_plugin_single_instance::init(|app, _, _| {
             utils::init::show_window(app);
         }))
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_store::Builder::default().build())
         .invoke_handler(tauri::generate_handler![
             command::cmds::open_window,
