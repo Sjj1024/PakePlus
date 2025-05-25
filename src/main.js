7e6ac22f91c24a3d840d8db221b7dd5f3078d167
4thdi <743689424@qq.com>
2025-05-25 16:33:47 +0800

    update src file from pakeplus

--- src/main.js
+++ src/main.js
@@ -1,19 +1,57 @@
-const { invoke } = window.__TAURI__.core
 
-let greetInputEl
-let greetMsgEl
-
-async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    // greetMsgEl.textContent = await invoke('greet', { name: greetInputEl.value })
-    console.log('greet')
+	window.onload = function(){
+			set_water_mark();
+			};
+	window.onresize = function(){
+		   set_water_mark();
+			};
+	function set_water_mark(){
+		let page_width = document.body.scrollWidth;
+		let page_height = document.body.scrollHeight;
+		
+		cols = parseInt(page_width/ (240 + 100))+1;
+		rows = parseInt(page_height/ (100 + 50))+1;
+		let x, y;
+		for (let i = 0; i < rows; i++){
+			y=30+(100+50) * i;
+			for(let j = 0; j < cols; j++){
+				x = 30 + (240+100) * j;
+		
+				var new_div = document.createElement('div');
+				new_div.innerHTML = "www.qytang.com<br>乾颐堂大路由出品";
+				new_div.style.transform = "rotate(-45deg)";
+				new_div.style.position = "absolute";
+				new_div.style.left = x + 'px';
+				new_div.style.top = y + 'px';
+				new_div.style.overflow = "hidden";
+				new_div.style.zIndex = "9999";
+				new_div.style.pointerEvents="none";
+				new_div.style.opacity = "0.2";
+				new_div.style.fontSize = "30px";
+				new_div.style.textAlign = "center";
+				new_div.style.height = "100px";
+				new_div.style.width = "245px";
+				new_div.style.color = "#ff6666";
+				document.body.appendChild(new_div);
+	}
+  }
 }
 
-window.addEventListener('DOMContentLoaded', () => {
-    greetInputEl = document.querySelector('#greet-input')
-    greetMsgEl = document.querySelector('#greet-msg')
-    document.querySelector('#greet-form').addEventListener('submit', (e) => {
-        e.preventDefault()
-        greet()
-    })
-})
+function stop(){
+return false;
+}
+document.oncontextmenu = stop;
+
+document.getElementsByTagName('img')[0].onmousedown = function(e){
+    e.preventDefault()
+};
+
+document.onmousemove=function(ev){
+　　var oEvent=ev||event;
+　　if(oEvent.preventDefault){
+        oEvent.preventDefault();
+    }else{
+        oEvent.returnValue=false;
+    }
+}
+
