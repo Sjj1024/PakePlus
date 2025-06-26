0ecd6a278fc52dd85fdfba2f4cd25e2c93e5c0cc
1024小神 <15670339118@163.com>
2025-06-26 11:46:19 +0800

    Update documentation to include GitHub account billing information and add a new method in github.ts for retrieving Bilibili user billing info. Remove unused common.ts file.

--- src/apis/github.ts
+++ src/apis/github.ts
@@ -272,4 +272,10 @@ export default {
             data: body,
         })
     },
+    // get bilibili info
+    getBilibiliInfo(user: string, month: string) {
+        return http(`/users/${user}/settings/billing/usage?month=${month}`, {
+            method: 'get',
+        })
+    },
 }
