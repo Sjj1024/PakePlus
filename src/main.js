4ccef4af10238b5a00589470890241bd8a412d13
1024小神 <15670339118@163.com>
2025-06-28 18:58:15 +0800

    Refactor index.html and main.js to enhance user interaction and functionality
    - Updated index.html to replace the greet input with a more generic input field and added multiple buttons for various commands.
    - Modified main.js to implement event listeners for new command buttons, enabling functionalities such as opening URLs, running commands, downloading files, retrieving executable directories, and fetching environment variables.
    - Added styling for new elements in styles.css to improve layout and user experience.
    - Removed unused SVG assets to clean up the project structure.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,75 @@
 const { invoke } = window.__TAURI__.core
 
-let greetInputEl
-let greetMsgEl
-
-async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-    // greetMsgEl.textContent = await invoke('greet', { name: greetInputEl.value })
-    console.log('greet')
-}
+let inputValue
+let resultElement
 
 window.addEventListener('DOMContentLoaded', () => {
-    greetInputEl = document.querySelector('#greet-input')
-    greetMsgEl = document.querySelector('#greet-msg')
-    document.querySelector('#greet-form').addEventListener('submit', (e) => {
+    inputValue = document.querySelector('#inputValue').value
+    resultElement = document.querySelector('#result')
+    // open url
+    document.querySelector('#openUrl').addEventListener('click', async (e) => {
         e.preventDefault()
-        greet()
+        console.log('open url')
+        await invoke('open_url', {
+            url: inputValue ? inputValue : 'https://juejin.cn/',
+        })
+    })
+    // run command
+    document
+        .querySelector('#runCommand')
+        .addEventListener('click', async (e) => {
+            e.preventDefault()
+            console.log('run command')
+            const result = await invoke('run_command', {
+                command: inputValue ? inputValue : 'node -v',
+            })
+            console.log('result', result)
+            resultElement.textContent = result
+        })
+    // download file
+    document
+        .querySelector('#downloadFile')
+        .addEventListener('click', async (e) => {
+            e.preventDefault()
+            console.log('download file')
+            const result = await invoke('download_file', {
+                url: inputValue
+                    ? inputValue
+                    : 'https://gh-proxy.com/github.com/Sjj1024/PakePlus/releases/latest/download/PakePlus_0.5.30_x64-setup.exe',
+                savePath: '',
+                fileId: '1111',
+            })
+            console.log('result', result)
+            resultElement.textContent = result
+        })
+    // get exe dir
+    document
+        .querySelector('#getExeDir')
+        .addEventListener('click', async (e) => {
+            e.preventDefault()
+            console.log('get exe dir')
+            const result = await invoke('get_exe_dir')
+            console.log('result', result)
+            resultElement.textContent = result
+        })
+    // get env var
+    document
+        .querySelector('#getEnvVar')
+        .addEventListener('click', async (e) => {
+            e.preventDefault()
+            console.log('get env var')
+            const result = await invoke('get_env_var', {
+                name: inputValue ? inputValue : 'PATH',
+            })
+            console.log('result', result)
+            resultElement.textContent = result
+        })
+    // find port
+    document.querySelector('#findPort').addEventListener('click', async (e) => {
+        e.preventDefault()
+        console.log('find port')
+        const result = await invoke('find_port')
+        console.log('result', result)
+        resultElement.textContent = result
     })
 })
