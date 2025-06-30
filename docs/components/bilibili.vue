c03d8131d02a7b47248efb36a9f5212b2b1f9ca5
1024小神 <15670339118@163.com>
2025-06-30 22:54:15 +0800

    Update BiliBili iframe permissions and increment version to 0.6.2 in latest.json and ppupdate.json. Modify platform URLs and signatures for new release, ensuring consistency across documentation.

--- docs/components/bilibili.vue
+++ docs/components/bilibili.vue
@@ -6,7 +6,7 @@
             :src="src"
             title="BiliBili video player"
             frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
         ></iframe>
     </div>
