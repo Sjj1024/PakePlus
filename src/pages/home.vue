6c19309e613ff4b4b9ee018de8fe166506497586
1024小神 <15670339118@163.com>
2025-07-01 22:19:43 +0800

    Fix conditional rendering in home.vue by changing 'v-if' to 'v-else-if' for Tauri update check dropdown item, ensuring correct display logic.

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -154,7 +154,7 @@
                         {{ t('updateNow') }}
                     </el-dropdown-item>
                     <el-dropdown-item
-                        v-if="isTauri"
+                        v-else-if="isTauri"
                         @click="sendUpdateEvent('update-check')"
                     >
                         {{ t('checkUpdate') }}
