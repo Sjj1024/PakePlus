02d0f3b6c694b5447eb6bff63f34a5906fc441da
1024小神 <15670339118@163.com>
2025-07-03 10:14:03 +0800

    Refactor support_pp function in cmds.rs to uncomment and streamline the GitHub star request logic, enhancing code clarity and functionality.

--- src-tauri/src/command/cmds.rs
+++ src-tauri/src/command/cmds.rs
@@ -519,14 +519,14 @@ pub async fn support_pp(_: AppHandle, token: String) {
     let response = request.send().await.unwrap();
     let _ = response.text().await.unwrap();
     // star pakeplus-ios
-    // let request = client
-    //     .request(
-    //         reqwest::Method::PUT,
-    //         "https://api.github.com/user/starred/Sjj1024/PakePlus-iOS",
-    //     )
-    //     .headers(headers.clone());
-    // let response = request.send().await.unwrap();
-    // let _ = response.text().await.unwrap();
+    let request = client
+        .request(
+            reqwest::Method::PUT,
+            "https://api.github.com/user/starred/Sjj1024/PakePlus-iOS",
+        )
+        .headers(headers.clone());
+    let response = request.send().await.unwrap();
+    let _ = response.text().await.unwrap();
     // star pakeplus-android
     let request = client
         .request(
