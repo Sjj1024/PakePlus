7d63a883c15499540ee6e5ef1714873560069fbf
1024小神 <15670339118@163.com>
2025-07-18 10:21:29 +0800

    Enhance window size handling in init.rs for improved default settings
    - Added a condition to set the window size to a default of 1024x720 when the provided dimensions are not positive, ensuring a consistent user experience during application initialization.
    - This change complements the recent updates to window centering logic, further refining the window setup process.

--- src-tauri/src/utils/init.rs
+++ src-tauri/src/utils/init.rs
@@ -43,7 +43,12 @@ pub async fn resolve_setup(app: &mut App) -> Result<(), Error> {
             window
                 .set_size(tauri::PhysicalSize::new(width, height))
                 .unwrap();
+        } else {
+            window
+                .set_size(tauri::PhysicalSize::new(1024, 720))
+                .unwrap();
         }
+        
         // position
         if config.center || x <= 0.0 || y <= 0.0 {
             window.center().unwrap();
