d12fd2ef6ee19b4e1a21d4170d756ba790a2910e
1024小神 <15670339118@163.com>
2025-07-28 22:10:21 +0800

    Refactor comments in ppweb.js to improve clarity and consistency, translating Chinese comments to English for better accessibility.

--- docs/public/ppweb.js
+++ docs/public/ppweb.js
@@ -1,14 +1,14 @@
 ;(async () => {
-    // 获取github release latest.json file
+    // get github release latest.json file
     const ppnotesJson = await fetch('/ppnotes.json')
     const ppnotesJsonData = await ppnotesJson.json()
     console.log('ppnotesJsonData', ppnotesJsonData)
     if (ppnotesJsonData.show) {
-        // 创建模态框元素
+        // create modal box element
         const modal = document.createElement('div')
         modal.className = 'modalBox'
 
-        // 模态框内容
+        // modal content
         modal.innerHTML = `
         <div class="modalContent">
             <h1 class="modalTitle">PakePlus公告</h1>
@@ -18,10 +18,10 @@
             </div>
         </div>
         `
-        // 添加到文档
+        // add to document
         document.body.appendChild(modal)
 
-        // 添加点击事件
+        // add click event
         document.getElementById('modalOk').addEventListener('click', () => {
             modal.remove()
             if (ppnotesJsonData.openUrl) {
