888899753a06b804820e9c0bf1bee84c2541749d
1024小神 <15670339118@163.com>
2025-09-25 22:14:19 +0800

    Update .gitignore to exclude 'dist' directory and include 'docs/dist' for better project organization

---
+++ dist/custom.js
@@ -0,0 +1,26 @@
+// very important, if you don't know what it is, don't touch it
+// 非常重要，不懂代码不要动，这里可以解决80%的问题，也可以生产1000+的bug
+const hookClick = (e) => {
+    const origin = e.target.closest('a')
+    const isBaseTargetBlank = document.querySelector(
+        'head base[target="_blank"]'
+    )
+    console.log('origin', origin, isBaseTargetBlank)
+    if (
+        (origin && origin.href && origin.target === '_blank') ||
+        (origin && origin.href && isBaseTargetBlank)
+    ) {
+        e.preventDefault()
+        console.log('handle origin', origin)
+        location.href = origin.href
+    } else {
+        console.log('not handle origin', origin)
+    }
+}
+
+window.open = function (url, target, features) {
+    console.log('open', url, target, features)
+    location.href = url
+}
+
+document.addEventListener('click', hookClick, { capture: true })
