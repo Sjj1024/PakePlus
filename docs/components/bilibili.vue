84c83458b2f5b6d89f69f5c51c11f2d6cef38d07
1024小神 <15670339118@163.com>
2025-10-04 18:55:59 +0800

    Enhance button styles in custom.css by adding bold font weight and update bilibili.vue to set iframe autoplay to false for improved user experience.

--- docs/components/bilibili.vue
+++ docs/components/bilibili.vue
@@ -8,6 +8,7 @@
             frameborder="0"
             allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
+            autoplay="false"
         ></iframe>
     </div>
 </template>
