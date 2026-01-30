e485e506d73eeb7996396f93556f6cd576e6ebdc
songjj <shongjj@meleapcn.com>
2026-01-30 18:48:00 +0800

    Add new Tauri plugins for file system, HTTP, OS, and process management in Cargo.toml and lib.rs. Update default.json to enhance window capabilities and permissions, including new dialog and process permissions.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -29,6 +29,10 @@ pub fn run() {
         })
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_store::Builder::default().build())
+        .plugin(tauri_plugin_process::init())
+        .plugin(tauri_plugin_fs::init())
+        .plugin(tauri_plugin_os::init())
+        .plugin(tauri_plugin_http::init())
         .invoke_handler(tauri::generate_handler![
             command::cmds::open_url,
             command::cmds::run_command,
