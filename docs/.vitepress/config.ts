27f3943fd067b7e6d615f04bf0875d23ac418270
Sjj1024 <15670339118@163.com>
2026-03-31 13:53:25 +0800

    sponsorship navigation and update active sponsorship documentation with additional notes on iOS packaging limitations.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -368,6 +368,7 @@ export default defineConfig({
                         { text: '赞助我们', link: '/zh/sponsor/' },
                         { text: '赞助者列表', link: '/zh/sponsor/list' },
                         { text: '赞助商名单', link: '/zh/sponsor/company' },
+                        { text: '开通权限', link: '/zh/sponsor/active' },
                     ],
                 },
                 // 社交链接
